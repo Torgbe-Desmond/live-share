ae476750a4cdae6c881de4887f98f8e03571b127
Torgbe-Desmond <123torgbe@gmail.com>
2026-02-19 18:06:08 +0000

    adjusted frontend api endpoint path

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(
   cors({
     origin: [
-      "https://live-share-frontend.vercel.app/",
+      "https://live-share-frontend.vercel.app",
       "http://localhost:3000",
       "http://localhost:56708",
     ],
