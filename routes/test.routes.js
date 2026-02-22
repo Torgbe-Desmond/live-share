086fbdc06f418275ca37607ab4b3ceadfed6454d
Torgbe-Desmond <123torgbe@gmail.com>
2026-02-22 22:07:00 +0000

    change test method from post to get

--- routes/test.routes.js
+++ routes/test.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.post("", (req, res) => {
+router.get("", (req, res) => {
   res.json({ message: "we are chatting!!!" });
 });
 
