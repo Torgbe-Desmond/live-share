a387033bca732c5a564dc1cdb267a561013f307c
123torgbe@gmail.com <123torgbe@gmail.com>
2026-06-06 09:52:37 +0000

    Frontend endpoint now in .env

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "https://live-share-frontend.vercel.app"],
+    origin: process.env.FRONTEND,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
